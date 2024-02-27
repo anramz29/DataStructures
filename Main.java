027666527115478c600aede7ca14592bb68f5f83
anramz29 <153331453+anramz29@users.noreply.github.com>
2024-02-27 14:12:45 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double[] array = new double["james", "tom","Tanner", "Sai", "Pawan", "John","Aidan","Joe","Logan"];
+
+
+
+    }
+}
